849dfb3c59afea76f7606ebbdad9fa7ae2e27472
phat.tran <phat.tran.fit@gmail.com>
2022-11-16 17:52:26 +0700

    - Add background hard image - Add to do list for code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const AppBg = styled.div`
+  background-image: url(/bg.jpg);
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position-y: center;
+  position: relative;
+`;
+const GameBg = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  background-color: #fff;
+  opacity: 0.3;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <AppBg className="App">
+    <GameBg />
+  </AppBg>;
 }
 
 export default App;
