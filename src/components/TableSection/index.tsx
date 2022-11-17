ebc98cfa0e4b2c08dc3dc9c26248aabf8a9a82e1
phat.tran <phat.tran.fit@gmail.com>
2022-11-17 12:33:54 +0700

    - Table hard color

---
+++ src/components/TableSection/index.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Stage, Layer, Ellipse } from 'react-konva';
+
+/* Function get darken or lighten color */
+const adjustColor = (color: string, percent: number) => {
+  let R = parseInt(color.substring(1, 3), 16);
+  let G = parseInt(color.substring(3, 5), 16);
+  let B = parseInt(color.substring(5, 7), 16);
+
+  R = parseInt(((R * (100 + percent)) / 100).toString());
+  G = parseInt(((G * (100 + percent)) / 100).toString());
+  B = parseInt(((B * (100 + percent)) / 100).toString());
+
+  R = R < 255 ? R : 255;
+  G = G < 255 ? G : 255;
+  B = B < 255 ? B : 255;
+
+  let RR = R.toString(16).length === 1 ? '0' + R.toString(16) : R.toString(16);
+  let GG = G.toString(16).length === 1 ? '0' + G.toString(16) : G.toString(16);
+  let BB = B.toString(16).length === 1 ? '0' + B.toString(16) : B.toString(16);
+
+  return `#${RR}${GG}${BB}`;
+};
+const greenHex = '#068117';
+const darkGreenHex = adjustColor(greenHex, -25);
+
+const GameSection: React.FC<{ themeColor: string }> = ({ themeColor }) => {
+  return (
+    <Stage width={1200} height={800}>
+      <Layer>
+        <Ellipse
+          x={600}
+          y={420}
+          radiusX={450}
+          radiusY={325}
+          fill={adjustColor(themeColor, -70)}
+        />
+        <Ellipse
+          x={600}
+          y={400}
+          radiusX={450}
+          radiusY={325}
+          fill={themeColor}
+        />
+        <Ellipse
+          x={600}
+          y={400}
+          radiusX={415}
+          radiusY={290}
+          fill={adjustColor(themeColor, -65)}
+        />
+        <Ellipse
+          x={600}
+          y={405}
+          radiusX={410}
+          radiusY={280}
+          fill={darkGreenHex}
+        />
+        <Ellipse
+          x={600}
+          y={405}
+          radiusX={405}
+          radiusY={275}
+          fill={darkGreenHex}
+          shadowColor="black"
+          shadowBlur={20}
+          shadowOpacity={0.5}
+        />
+        <Ellipse
+          x={600}
+          y={405}
+          radiusX={365}
+          radiusY={235}
+          fill={greenHex}
+          stroke={adjustColor(themeColor, 50)}
+          strokeWidth={8}
+        />
+      </Layer>
+    </Stage>
+  );
+};
+
+export default GameSection;
